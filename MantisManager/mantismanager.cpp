d7122e3a422104dfe4bc6e73e36a23c0a3482eb2
Ambrou <apetitgenet@free.fr>
2015-11-14 12:35:34 +0100

    Création du projet

---
+++ MantisManager/mantismanager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "mantismanager.h"
+
+MantisManager::MantisManager()
+{
+
+}
+
+MantisManager::~MantisManager()
+{
+
+}
---
+++ MantisManager/mantismanager.h
@@ -0,0 +1,16 @@
+#ifndef MANTISMANAGER_H
+#define MANTISMANAGER_H
+
+#include "mantismanager_global.h"
+
+class MANTISMANAGER_EXPORT MantisManager
+{
+public:
+	MantisManager();
+	~MantisManager();
+
+private:
+
+};
+
+#endif // MANTISMANAGER_H

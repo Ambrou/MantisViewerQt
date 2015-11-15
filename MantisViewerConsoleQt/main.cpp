49897dd660180fc554cd6ba830e38828afe18562
Ambrou <apetitgenet@free.fr>
2015-11-15 18:41:41 +0100

    Création du projet de test

--- MantisViewerConsoleQt/main.cpp
+++ MantisViewerConsoleQt/main.cpp
@@ -1,9 +1,11 @@
-
+#include "MantisViewerConsoleQt.h"
 #include <QtCore/QCoreApplication>
 
 int main(int argc, char *argv[])
 {
-	QCoreApplication a(argc, argv);
-
-	return a.exec();
+	QCoreApplication app(argc, argv);
+	MantisViewerConsoleQt console(&app);
+	console.start();
+	QObject::connect(&console, SIGNAL(quitter()), &app, SLOT(quit()));
+	return app.exec();
 }

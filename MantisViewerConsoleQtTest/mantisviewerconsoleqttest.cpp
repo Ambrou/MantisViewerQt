d86dd640c52dd16b0d2a0c48e052320b2afa7350
Ambroise Petitgenêt <apetitgenet@free.fr>
2015-11-21 12:42:08 +0100

    Rajout du projet de test de la console

---
+++ MantisViewerConsoleQtTest/mantisviewerconsoleqttest.cpp
@@ -0,0 +1,11 @@
+#include "mantisviewerconsoleqttest.h"
+
+MantisViewerConsoleQtTest::MantisViewerConsoleQtTest()
+{
+
+}
+
+MantisViewerConsoleQtTest::~MantisViewerConsoleQtTest()
+{
+
+}
---
+++ MantisViewerConsoleQtTest/mantisviewerconsoleqttest.h
@@ -0,0 +1,16 @@
+#ifndef MANTISVIEWERCONSOLEQTTEST_H
+#define MANTISVIEWERCONSOLEQTTEST_H
+
+#include "mantisviewerconsoleqttest_global.h"
+
+class MANTISVIEWERCONSOLEQTTEST_EXPORT MantisViewerConsoleQtTest
+{
+public:
+	MantisViewerConsoleQtTest();
+	~MantisViewerConsoleQtTest();
+
+private:
+
+};
+
+#endif // MANTISVIEWERCONSOLEQTTEST_H

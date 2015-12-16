92d2ba9818b7cbccd244b1db3636638f84fdfb1d
Ambroise Petitgenêt <ptit_ambrou@yahoo.fr>
2015-12-16 13:11:25 +0000

    Exportation de la création de ticket et de vesion

--- MantisManager/BaseConnecteur.h
+++ MantisManager/BaseConnecteur.h
@@ -14,7 +14,9 @@ public:
 	virtual void recupererVersionsDuProjet(QVector<QString>&listeVersion, const QString nomDuProjet, const QString& user, const QString& password) const = 0;
 	virtual void recupererTicketDeLaVersionsDuProjet(QVector<QString>&listeVersion, const QString nomDuProjet, const QString nomVersion, const QString& user, const QString& password) const = 0;
 	virtual void changerEtatTicket(const QString& idTicket, const QString& nouvelEtat, const QString& user, const QString& password) const = 0;
-	//virtual void créerUn
+	virtual void creerUnTicket(const QString& nomDuProjet, const QString& description, const QString& resume, const QString& categorie, const QString& user, const QString& password) const = 0;
+	virtual void creerUneVersion(const QString& nomVersion, const QString& projet, const QString& user, const QString& password) const = 0;
+	//virtual void crï¿½erUn
 	virtual void recupererTout() const = 0;
 };
 

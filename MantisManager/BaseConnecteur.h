4600a6e2673d648a12ff59a54df023e4324370bb
Ambroise Petitgenêt <apetitgenet@free.fr>
2016-01-23 10:45:22 +0100

    Rajout de l'interface livrer une version

--- MantisManager/BaseConnecteur.h
+++ MantisManager/BaseConnecteur.h
@@ -19,6 +19,7 @@ public:
 	virtual void creerUnTicket(const QString& nomDuProjet, const QString& description, const QString& resume, const QString& categorie, const QString& user, const QString& password) const = 0;
 	virtual void creerUneVersion(const QString& nomVersion, const QString& projet, const QString& user, const QString& password) const = 0;
 	virtual void definirDateLivraisonDUneVersion(const QString& version, const QDate& date, const QTime& time, const QString& projet, const QString& user, const QString& password) const = 0;
+	virtual void livrerVersion(const QString& version, const QString& projet, const QString& user, const QString& password) const = 0;
 	virtual void recupererTout() const = 0;
 };
 
